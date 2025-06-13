47658f34255c09054f9043cd2c55f0259dadaea2
The Fckin' Boss <iamthenobel@gmail.com>
2025-06-13 12:35:18 +0100

    Initial commit

--- server/server.js
+++ server/server.js
@@ -193,4 +193,5 @@ app.delete('/api/notes/:id', authenticateToken, (req, res) => {
 });
 
 // Start server
-app.listen(4000, () => console.log('Server running on http://localhost:4000'));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
